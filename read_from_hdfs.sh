7193d8c77638d94035a0ae168cb561a053c9ca3d
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-06-11 19:17:11 +0530

    Dynamically Read From All HDFS Datanodes

--- read_from_hdfs.sh
+++ read_from_hdfs.sh
@@ -1,19 +1,59 @@
 #!/bin/bash
 
 NAMESPACE="ndl"
-DATANODE_POD_NAME="ndl-hdfs-datanode-0"
 HDFS_FILE="/tmp/data.txt"
-LOCAL_FILE="downloaded_data.txt"
 
-file_exists=$(kubectl exec -n $NAMESPACE $DATANODE_POD_NAME -- /bin/bash -c "test -f $HDFS_FILE && echo yes || echo no")
+function read_data_from_pod {
+  local pod_name="$1"
 
-if [ "$file_exists" == "yes" ]; then
-  rm -rf $LOCAL_FILE
-  start_time=$(date +%s.%N)
-  kubectl cp -n $NAMESPACE $DATANODE_POD_NAME:$HDFS_FILE $LOCAL_FILE
-  end_time=$(date +%s.%N)
-  total_time=$(echo "$end_time" "$start_time" | awk '{print $1 - $2}')
-  echo "File '$HDFS_FILE' copied successfully to '$LOCAL_FILE' in $total_time seconds (approximately)"
-else
-  echo "File '$HDFS_FILE' does not exist on the DataNode pod."
+  if kubectl exec -n "$NAMESPACE" "$pod_name" -- test -f "$HDFS_FILE"; then
+    local start_time=$(date +%s.%N)
+    local local_file="downloaded_data_${pod_sequence_number}.txt"
+    kubectl cp -n "$NAMESPACE" "$pod_name:$HDFS_FILE" "$local_file"
+    local end_time=$(date +%s.%N)
+    local read_time=$(echo "$end_time" "$start_time" | awk '{print $1 - $2}')
+    echo "File '$HDFS_FILE' copied from '$pod_name' to '$local_file' in $read_time seconds (approximately)"
+    return $read_time
+  else
+    echo "File '$HDFS_FILE' does not exist on the DataNode pod '$pod_name'."
+    return 0
+  fi
+}
+
+pod_names=( $(kubectl get pods -n "$NAMESPACE" --no-headers | grep datanode | awk '{print $1}') )
+
+if [[ ${#pod_names[@]} -eq 0 ]]; then
+  echo "Error: No DataNode pods found in namespace '$NAMESPACE'."
+  exit 1
 fi
+
+read_times=()
+
+pod_sequence_number=1
+
+overall_start_time=$(date +%s.%N)
+
+for pod in "${pod_names[@]}"; do
+  echo "Reading data from pod: $pod"
+  read_time=$(read_data_from_pod "$pod")
+  read_times[((pod_sequence_number-1))]=$read_time
+  ((pod_sequence_number++))
+done
+
+overall_end_time=$(date +%s.%N)
+
+overall_time=$(echo "$overall_end_time" "$overall_start_time" | awk '{print $1 - $2}')
+
+if [[ ${#read_times[@]} -gt 0 ]]; then
+  echo ""
+  echo "** Individual Pod Read Times (seconds):"
+  for ((i=0; i<${#pod_names[@]}; i++)); do
+    pod="${pod_names[$i]}"
+    time=${read_times[$i]}
+    if [[ ! -z "$time" ]]; then
+      echo "- Pod: $pod - Time: $time"
+    fi
+  done
+fi
+
+echo "** Overall time spent reading data from all pods: $overall_time seconds **"
