9e7847ddefd362e0faf5397896d70b861bd107b4
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-06-11 19:04:48 +0530

    Read POD Names Dynamically and Store Data In HDFS Datanode PODs

--- write_to_hdfs.sh
+++ write_to_hdfs.sh
@@ -18,18 +18,25 @@ function write_data_to_pod {
 
   total_time=$(echo "$end_time" "$start_time" | awk '{print $1 - $2}')
 
-  echo "Total time spent writing data to '$pod_name': $total_time seconds (approximately)"
+  echo "Total time spent writing data to '$pod_name': $total_time seconds"
 }
+
+pod_names=( $(kubectl get pods -n "$NAMESPACE" --no-headers | grep datanode | awk '{print $1}') )
+
+if [ ${#pod_names[@]} -eq 0 ]; then
+  echo "Error: No DataNode pods found in namespace '$NAMESPACE'."
+  exit 1
+fi
 
 overall_start_time=$(date +%s.%N)
 
-for pod in "$@"; do
+for pod in "${pod_names[@]}"; do
   echo "Writing data to pod: $pod"
   write_data_to_pod "$pod"
 done
 
 overall_end_time=$(date +%s.%N)
 
-overall_time=$(echo "$overall_end_time" "$overall_start_time" | awk '{print $1 - $2}')
+overall_time=$(echo "$end_time" "$overall_start_time" | awk '{print $1 - $2}')
 
-echo "** Overall time spent writing data to all pods: $overall_time seconds (approximately) **"
+echo "** Overall time spent writing data to all pods: $overall_time seconds **"
