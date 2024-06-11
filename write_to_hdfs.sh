8c3aa86345ae8f6d1a58a0daecc34b7e3d6ba255
Subhradeep Ray <35240569+sraynitjsr@users.noreply.github.com>
2024-06-11 18:47:24 +0530

    Writing To HDFS Datanode PODs and Calculating Total Time Taken

--- write_to_hdfs.sh
+++ write_to_hdfs.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
 NAMESPACE="ndl"
-DATANODE_POD_NAME="ndl-hdfs-datanode-0"
 TEMP_FILE="/tmp/data.txt"
 
-start_time=$(date +%s.%N)
+function write_data_to_pod {
+  local pod_name="$1"
 
-for i in {1..100}; do
-  current_time=$(date "+%Y-%m-%d %H:%M:%S")
-  data="Hello World $i - $current_time"
-  kubectl exec -it $DATANODE_POD_NAME -n $NAMESPACE -- /bin/bash -c "echo \"$data\" >> $TEMP_FILE"
+  start_time=$(date +%s.%N)
+
+  for i in {1..100}; do
+    current_time=$(date "+%Y-%m-%d %H:%M:%S")
+    data="Hello World $i - $current_time"
+    kubectl exec -it $pod_name -n $NAMESPACE -- /bin/bash -c "echo \"$data\" >> $TEMP_FILE"
+  done
+
+  end_time=$(date +%s.%N)
+
+  total_time=$(echo "$end_time" "$start_time" | awk '{print $1 - $2}')
+
+  echo "Total time spent writing data to '$pod_name': $total_time seconds (approximately)"
+}
+
+overall_start_time=$(date +%s.%N)
+
+for pod in "$@"; do
+  echo "Writing data to pod: $pod"
+  write_data_to_pod "$pod"
 done
 
-end_time=$(date +%s.%N)
+overall_end_time=$(date +%s.%N)
 
-total_time=$(echo "$end_time" "$start_time" | awk '{print $1 - $2}')
+overall_time=$(echo "$overall_end_time" "$overall_start_time" | awk '{print $1 - $2}')
 
-echo "Total time spent writing data: $total_time seconds (approximately)"
+echo "** Overall time spent writing data to all pods: $overall_time seconds (approximately) **"
